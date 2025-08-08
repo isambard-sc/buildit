e8f9c1a81b36e9d64519af2cc39fb1ba24cffa57
green-br <thomas.green@bristol.ac.uk>
2025-08-08 16:53:06 +0100

    Add gromacs example (#34)
    * Includes fix for Gromacs 2025 cmake requirement.

---
+++ repo/v1.0/spack_repo/isamrepo/packages/gromacs/package.py
@@ -0,0 +1,13 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package_base import PackageBase # Or other base class
+
+# Import the original Mpich class from the 'builtin' repository
+from spack_repo.builtin.packages.gromacs.package import Gromacs as BuiltinGromacs
+
+from spack.package import *
+
+class Gromacs(BuiltinGromacs):
+    depends_on("cmake@3.28:3", type="build", when="@2025:")
